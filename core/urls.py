27081d84d6b88ad8e546308e0ceb556babd1d272
dev <mdanarul7075@gmail.com>
2023-09-23 03:35:14 +0600

    added: urls country and division model view register

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+schema_view = get_schema_view(
+    openapi.Info(
+        title="BD Smart School Management",
+        default_version="v1",
+        description="BD Smart School Management api provide for frontend. It's protected and just our business purpose.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="mdanarul7075@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    # permission_classes=[permissions.AllowAny],
+)
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include([path("", include("apps.addresses.urls"))])),
+    path(
+        "",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("docs/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
