97f513511e59c36aae99efee9017400ae40c318e
dev <mdanarul7075@gmail.com>
2023-09-22 23:16:27 +0600

    update: all django app move in apps folder and app updated

--- apps/klass/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class KlassConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "klass"
--- apps/libraries/apps.py
+++ apps/libraries/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LibrariesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "libraries"
+    name = "apps.libraries"
