55b40466d56d5533c0e2f9b8e1355b9042366a35
dev <mdanarul7075@gmail.com>
2023-09-23 03:36:18 +0600

    added: address all models modelviewset added

--- apps/addresses/views.py
+++ apps/addresses/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import (
+    TokenAuthentication,
+    BaseAuthentication,
+    SessionAuthentication,
+)
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from apps.helpers.mixins import DeleteModelMixin
+from apps.addresses.models import (
+    Country,
+    Division,
+    District,
+    SubDistrict,
+    Union,
+    Word,
+    Address,
+)
+from apps.addresses.serializers import (
+    CountrySerializer,
+    DivisionSerializer,
+    DistrictSerializer,
+    SubDistrictSerializer,
+    UnionSerializer,
+    WordSerializer,
+    AddressSerializer,
+)
 
-# Create your views here.
+
+class CountyModelViewSet(DeleteModelMixin, ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+
+class DivisionModelViewSet(DeleteModelMixin, ModelViewSet):
+    queryset = Division.objects.all()
+    serializer_class = DivisionSerializer
+
+
+class DistrictModelViewSet(DeleteModelMixin, ModelViewSet):
+    queryset = District.objects.all()
+    serializer_class = DistrictSerializer
+
+
+class SubDistrictModelViewSet(DeleteModelMixin, ModelViewSet):
+    queryset = SubDistrict.objects.all()
+    serializer_class = SubDistrictSerializer
+
+
+class UnionModelViewSet(DeleteModelMixin, ModelViewSet):
+    queryset = Union.objects.all()
+    serializer_class = UnionSerializer
+
+
+class WordModelViewSet(DeleteModelMixin, ModelViewSet):
+    queryset = Word.objects.all()
+    serializer_class = WordSerializer
+
+
+class AddressModelViewSet(DeleteModelMixin, ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
