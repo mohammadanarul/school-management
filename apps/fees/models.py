287f44e3f2e2929d11f5e597ef242e13ff828c8c
dev <mdanarul7075@gmail.com>
2023-09-22 23:35:42 +0600

    added: fee model add

--- apps/fees/models.py
+++ apps/fees/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from apps.helpers.models import BaseEntity
+from apps.helpers.utils import FeeTypes, FeeStatus
+from apps.users.models import User
 
-# Create your models here.
+
+class Fee(BaseEntity):
+    user = models.ForeignKey(
+        User, verbose_name=_("user"), on_delete=models.CASCADE, related_name="fees"
+    )
+    object_id = models.BigIntegerField(null=True, blank=True)
+    transaction_id = models.CharField(_("transaction id"), max_length=100)
+    balance = models.DecimalField(_("balance"), decimal_places=2, max_digits=6)
+    type = models.SmallIntegerField(_("type"), choices=FeeTypes.choices)
+    status = models.SmallIntegerField(_("status"), choices=FeeStatus.choices)
+
+    def __str__(self):
+        return self.get_type_display()
