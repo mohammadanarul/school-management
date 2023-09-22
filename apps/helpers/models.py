dc102327fc692e33b0ef821068d11275baafc0ac
dev <mdanarul7075@gmail.com>
2023-09-23 03:31:43 +0600

    updated: model updated created_by adn updated_by field comment

--- apps/helpers/models.py
+++ apps/helpers/models.py
@@ -1,16 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import User
+
+# from django.contrib.auth import get_user_model
+#
+# User = get_user_model()
 
 
 class BaseEntity(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(
-        User, on_delete=models.SET_NULL, related_name="+", null=True, blank=True
-    )
-    updated_by = models.ForeignKey(
-        User, on_delete=models.SET_NULL, related_name="+", null=True, blank=True
-    )
+    # created_by = models.ForeignKey(
+    #     User, on_delete=models.SET_NULL, related_name="+", null=True, blank=True
+    # )
+    # updated_by = models.ForeignKey(
+    #     User, on_delete=models.SET_NULL, related_name="+", null=True, blank=True
+    # )
 
     class Meta:
         abstract = True
