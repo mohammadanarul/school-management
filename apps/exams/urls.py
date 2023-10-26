8f7e3cb30848e25c3a6e088ad22dcfb575169640
Mohammad Anarul Islam <43848277+mohammadanarul@users.noreply.github.com>
2023-10-27 01:45:01 +0600

    add: exam_results api added

--- apps/exams/urls.py
+++ apps/exams/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
-from apps.exams.views import ExamModelViewSet
+from apps.exams.views import ExamModelViewSet, ExamResultModelViewSet
 
 
 router = routers.DefaultRouter()
 
 router.register("exams", ExamModelViewSet, basename="exams")
+router.register("exam_results", ExamResultModelViewSet, basename="exam_results")
 
 urlpatterns = [] + router.urls
