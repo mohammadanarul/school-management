b5520e80da8f336352d5091aac4c6d3f4b91ae63
Mohammad Anarul Islam <43848277+mohammadanarul@users.noreply.github.com>
2023-10-27 01:42:46 +0600

    new-feature: exam result model view set added

--- apps/exams/views.py
+++ apps/exams/views.py
@@ -1,8 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAdminUser
-from apps.exams.models import Exam
-from apps.exams.serializers import ExamSerializer
+from apps.exams.models import Exam, ExamResult
+from apps.exams.serializers import ExamSerializer, ExamResultSerializer
 
 
 class ExamModelViewSet(ModelViewSet):
@@ -10,3 +10,10 @@ class ExamModelViewSet(ModelViewSet):
     permission_classes = [IsAdminUser]
     queryset = Exam.objects.all()
     serializer_class = ExamSerializer
+
+
+class ExamResultModelViewSet(ModelViewSet):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]
+    queryset = ExamResult.objects.all()
+    serializer_class = ExamResultSerializer
