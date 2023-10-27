0af14f8fd4186f5940d1c1d45dfef1b433093c77
Mohammad Anarul Islam <43848277+mohammadanarul@users.noreply.github.com>
2023-10-27 11:52:30 +0600

    update: action wise permission update

--- apps/exams/views.py
+++ apps/exams/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated, DjangoModelPermissions
 from apps.exams.models import Exam, ExamResult
 from apps.exams.serializers import ExamSerializer, ExamResultSerializer
 
@@ -17,3 +17,8 @@ class ExamResultModelViewSet(ModelViewSet):
     permission_classes = [IsAdminUser]
     queryset = ExamResult.objects.all()
     serializer_class = ExamResultSerializer
+
+    def get_permissions(self):
+        if self.action == "list" or self.action == "retrieve":
+            return [IsAuthenticated()]
+        return [DjangoModelPermissions()]
